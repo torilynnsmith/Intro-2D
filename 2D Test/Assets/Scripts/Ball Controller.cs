1471635bd5cbb02cca172255cfaed1696b9ab9b5
Tori Smith <torilynnsmith@gmail.com>
2023-09-04 12:21:45 -0400

    pong setup & ball start

---
+++ 2D Test/Assets/Scripts/Ball Controller.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    //Declare Universal Variables 
+    //public Rigidbody2D rb; //get Rigidbody 2D component from sprite
+    private Rigidbody2D rb; //get Rigidbody 2D privately
+    public float ballSpeed = 5; //declare ballSpeed variable
+    //private float xPos; //get x position
+    //private float yPos; //get y position
+    private float xDir; //declare x direction
+    private float yDir; //declare y direction
+   
+    //Vector2 direction; //Vector2 = representation of 2D vectors and points
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1. Add a Debug Message
+        Debug.Log("Hello World");
+
+
+        rb = GetComponent<Rigidbody2D>(); 
+            //get the component of the gameobject the script is placed on
+            //comment after showing the global variable
+
+        //Make Ball Move in Random Direction at start
+        xDir = Random.Range(0, 2) == 0 ? -1 : 1; //set x direction
+            //if x = -1, it'll move right. If x = 1, it'll move left
+        yDir = Random.Range(0, 2) == 0 ? -1 : 1; //set y direction
+                                                 //if y = -1, it'll move down. If y = 1, it'll move up
+
+        Vector2 direction = new Vector2(xDir, yDir); //create new Vector2 variable
+        //rb.velocity = new Vector2(ballSpeed * xDir, ballSpeed * yDir); //create a force on the ball
+        rb.velocity = direction * ballSpeed; //essentially the same as above, just loading the direction variables into one value
+
+        //Make Ball Move at Start (fancier angles) 
+        //float x = Random.value < 0.5f ? -1.0f : 1.0f; //x coor value, choose left or right
+        //if x is less than half, it'll be one direction, if greater then it is the other
+        //float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f):Random.Range(0.5f, 1.0f); //y coor value, choose random angle
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
