fb4e358d96031a25edda9b995e5474a614fc02c3
Tori Smith <torilynnsmith@gmail.com>
2023-11-29 13:25:08 -0500

    singleton set up

--- 2D Platformer/Assets/Scripts/Tori's Scripts/MakeNoise.cs
+++ 2D Platformer/Assets/Scripts/Tori's Scripts/MakeNoise.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class MakeNoise : MonoBehaviour
 {
-    //This is an Audio Manager! Attach it to an empty Game Object named Audio Manager
-    //Play Background Music in Unity that doesn't destroy b/w scenes
-    //1. Show how to make music automatically play on awake version first.
+    //This is a singleton demo we're using to maintain a Game Manager! Attach it to an empty Game Manager Object in your first/starting scene
 
+    //Play Background Music in Unity that doesn't destroy, restart, and/or overlap b/w scenes
+    //1. Show how to make music automatically play on awake version first (attach to main camera)
+    //2. Attach music to main camera in start scene to demonstrate the music restarting when the new scene loads
+    //3. Write the following code to explain singletons
+    //4. Attach music to Game Manager Object in start scene to demonstrate overlapping.
+    //5. Attach music to Game Manager Object in main scene to demonstrate the singleton destroying the duplicate object and not restarting the music
+    //6. reiterate how this is helpful for maintaining static info b/w scenes and how it reduces the need to add a game manager into every scene now 
+   
     //GLOBAL VARIABLES
-    //music
 
-    //you want to declare instances so that you don't duplicate Audio Manager objects (if you happened to have different ones across your scenes)
-    private static MakeNoise instance = null; //static variables are shared by all instances of a class. This means it preserves its values when "out of scope" or outside of the loaded scene
-    public static MakeNoise Instance //
-    {
-        get { return instance; } //WHERE I LEFT OFF. ADD NOTES ON WHAT THIS DOES HERE
-    }
+    //Singletons:you want to declare instances so that you don't duplicate the Audio Manager objects (if you happened to have different ones across your scenes)
+    public static GameObject instance; //static variables are shared by all instances of a class. This means it preserves its values when "out of scope" or outside of the loaded scene
+        //NOTE: even though this is a public static variable, it will not appear in the inspector. It is set through code by its mere existence
 
 
     // Start is called before the first frame update
@@ -24,13 +26,28 @@ public class MakeNoise : MonoBehaviour
     {
         //you can always get the component through code instead of the inspector
         //source = GetComponent<AudioSource>(); //get the audio source component at start from the object this script is attached to. 
+        //Debug.Log("printed at start"); 
     }
 
     //Awake is called once when the script instance is loaded
     private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject); 
-        //don't destory the target object when loading a new Scene
+        //Debug.Log("printed at Awake");
+
+        //when the new scene/instance of this script loads...
+        if (instance == null) //if the instance of the game object this script is attached to does not exist when the new scene starts....
+        {
+            Debug.Log("Game Manager not destroyed."); //print to console
+            instance = gameObject; //the new static instance becomes equal to this game object. It now exists! 
+            DontDestroyOnLoad(gameObject); //don't destory the target object when loading a new Scene
+        }
+        else //if the instance DOES already exist in the scene...
+        {
+            Debug.Log("Game Manager DESTROYED."); //print to console
+            Destroy(gameObject); //destroy the duplicate gameObject in the new scene so you don't have duplicates of things floating around and running code twice
+        }
+        //The above code is useful for maintaining information between scenes. This can apply to things like:
+        //game managers, background music, save states, scores, etc. when moving b/w scenes.
     }
 
 
@@ -39,7 +56,5 @@ public class MakeNoise : MonoBehaviour
     {
         
     }
-
-    //Play the Sound
 
 }
---
+++ 2D Platformer/Assets/Scripts/Tori's Scripts/Scene Changer.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //use Scene Management Library
+
+//THIS IS TORI'S SCRIPT
+
+public class SceneChanger : MonoBehaviour
+{
+    //GLOBAL VARIALBES
+
+    //public KeyCode sceneKey; //set key to change scene in Inspector
+    public int sceneNumber; //set Scene Number in Inspector
+                            //Start = 1, Main = 2, End = 3
+
+    static int testNumber; //you have to make it static to carry the value b/w scenes
+    public int testNumberDisplay; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("printed at start. testNumber = " + testNumber); //print to console
+
+    }
+
+    private void Awake()
+    {
+        Debug.Log("printed at awake. testNumber = " + testNumber); //print to console
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        testNumberDisplay = testNumber; 
+        //BASIC KEY CHECK CODE -> MOVE TO SPECIFIC SCENE FUNCTIONS WHEN DONE
+        //if (Input.GetKey(KeyCode.Space)) //if Space is pressed, called as long as key is pressed
+        //if (Input.GetKeyDown(KeyCode.Space)) //if Space is pressed, called only once
+        ////if (Input.GetKeyDown(sceneKey)) //if sceneKey is pressed, called only once
+        //{
+        //    //Debug.Log("Space pressed"); //print to console
+        //    //Debug.Log("sceneKey pressed"); //print to console
+        //    EndScene(); //call EndScene() function
+        //}
+        if (sceneNumber == 1) //if Start Scene
+        {
+            MainScene(); //call MainScene() / Load Main Scene
+        } else if (sceneNumber == 2) //if Main Scene
+        {
+            EndScene(); //call EndScene() / Load End Scene
+        } else if (sceneNumber == 3) //if End Scene
+        {
+            StartScene(); //call Start Scene /Load Start Scene
+        }
+    }
+
+    public void StartScene() //go to Start Scene
+    {
+        //Debug.Log("StartScene() Called"); //print to console
+        //SceneManager.LoadScene("Start Scene"); //Load Start Scene
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Debug.Log("StartScene() Called"); //print to console
+            sceneNumber = 1;
+            testNumber++; //add 1 to testNumber
+            SceneManager.LoadScene("Tori's Start Scene"); //Load Start Scene
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            //Debug.Log("MainScene() Called"); //print to console
+            sceneNumber = 2;
+            testNumber++; //add 1 to testNumber
+            SceneManager.LoadScene("Tori's Level 1"); //Load Main Scene
+        }
+    }
+
+    public void MainScene() //go to Main Scene
+    {
+        //Debug.Log("MainScene() Called"); //print to console
+        //SceneManager.LoadScene("Main Scene"); //Load Main Scene
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //Debug.Log("MainScene() Called"); //print to console
+            sceneNumber = 2;
+            testNumber++; //add 1 to testNumber
+            SceneManager.LoadScene("Tori's Level 1"); //Load Main Scene
+        }
+    }
+
+    public void EndScene() //go to End Scene when a key is pressed
+    {
+        //if (Input.GetKey(KeyCode.Space)) //if Space is pressed, called as long as key is pressed
+        if (Input.GetKeyDown(KeyCode.Space)) //if Space is pressed, called only once
+        //if (Input.GetKeyDown(sceneKey)) //if sceneKey is pressed, called only once
+        {
+            //Debug.Log("EndScene() Called"); //print to console
+            sceneNumber = 3;
+            testNumber++; //add 1 to testNumber
+            SceneManager.LoadScene("Tori's End Scene"); //Load End Scene
+        }
+        //Debug.Log("EndScene() Called"); //print to console
+        //SceneManager.LoadScene("End Scene"); //Load End Scene
+                                             //NOTE: You need to import all the scenes you're switching between
+                                             //in your build settings before this will work!
+    }
+
+    //CHANGE SCENE W/ BUTTON AND SCENE ID.
+    //This is the more efficient way w/o having to write a specific new function for each scene change (which we already have for keycode checks)
+    public void MoveToScene(int sceneID) //added in a "value parameter", similar to our Collision 2D code!
+        //Here, I've declared and am grabbing values for a new int variable "sceneID"
+    {
+        //NOTE: You will need to add the default Event System Object back to your hierarchy to recognize mouse clicks
+
+        //Debug.Log("MoveToScene Called"); //print to console
+        //SceneManager.LoadScene("End Scene"); //call a scene specifically by it's name
+        //OR
+        SceneManager.LoadScene(sceneID); //load a scene based on its Scene ID # in Build Settings.
+            //Set this number in the Inspector
+    }
+
+}
